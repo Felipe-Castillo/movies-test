68492b065c76071ae7abb5589f3f20a2dddf849a
felipe castillo <felcast999@gmail.com>
2021-08-04 11:15:28 -0400

    relaciones

--- app/Models/Genre.php
+++ app/Models/Genre.php
@@ -7,4 +7,23 @@ use Illuminate\Database\Eloquent\Model;
 class Genre extends Model
 {
     //
+
+    use SoftDeletes;
+
+    protected $table="genres";
+
+    protected $fillable=["name"];
+
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'publication_date'
+    ];
+
+
+      public function movies()
+    {
+    	return $this->hasMany(Shift::class,'genre_id');
+    }
+
 }
--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -3,8 +3,32 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Movie extends Model
 {
-    //
+   
+    use SoftDeletes;
+
+    protected $table="movies";
+
+    protected $fillable=["name","publication_date","status","genre_id"];
+
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'publication_date'
+    ];
+
+
+      public function shifts()
+    {
+    	return $this->belongsToMany(Shift::class,'movie_shift','movie_id','shift_id');
+    }
+
+    public function genre()
+    {
+        return $this->belongsTo(Genre::class,'genre_id');
+    }
+
 }
