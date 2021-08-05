6a48e5095db1859388a6efbf733f846a2e12b2e4
felipe castillo <felcast999@gmail.com>
2021-08-05 11:11:08 -0400

    init

--- routes/api.php
+++ routes/api.php
@@ -17,28 +17,3 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-
-Route::post('login', 'AuthController@login');
-Route::post('register', 'AuthController@register');
-
-
-Route::post('logout', 'AuthController@logout');
-Route::post('refresh', 'AuthController@refresh');
-Route::get('me', 'AuthController@me');
-
-Route::resources([
-
-        // peliculas
-        '/movies'          => 'MovieController', 
-
-        // turnos
-        '/shifts'          => 'ShiftController', 
-
-        
-    ]);
-
-//movies
-Route::get('api-movies', 'MovieController@get');
-
-
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return redirect('/index.html');
+    return view('welcome');
 });
